9097e773026926f2866f1b6a09fecb6348a36b3c
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-01-28 20:36:35 +0530

    array types in ts

--- week-9-typescript/node-app/src/a.ts
+++ week-9-typescript/node-app/src/a.ts
@@ -1,17 +1,36 @@
-type Employee = {
-  name: string;
-  startDate: Date;
-};
+function maxValue(arr: number[]) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
 
-interface Manager  {
-  name: string;
-  department: string;
-};
+console.log(maxValue([1, 2, 3]));
 
-type TeamLead = Employee & Manager;
+interface User {
+  firstName: string;
+  lastName: string;
+  age: number;
+}
 
-const teamLead: TeamLead = {
-  name: "harkirat",
-  startDate: new Date(),
-  department: "Software developer"
-};
+function filteredUsers(users: User[]) {
+  return users.filter((x) => x.age >= 18);
+}
+
+console.log(
+  filteredUsers([
+    {
+      firstName: "harkirat",
+      lastName: "Singh",
+      age: 21,
+    },
+    {
+      firstName: "Raman",
+      lastName: "Singh",
+      age: 16,
+    },
+  ])
+);
