b856ef9261b5a3863783b575aade2e7d9c178a48
C0dewithLokesh <lokeshkaushik2243@gmail.com>
2024-01-28 20:24:00 +0530

    types in ts

--- week-9-typescript/node-app/src/a.ts
+++ week-9-typescript/node-app/src/a.ts
@@ -1,19 +1,5 @@
-interface Person {
-  name: string;
-  age: number;
-  greet(phrase: string): void;
-}
-
-class Employee implements Person {
-  name: string;
-  age: number;
-
-  constructor(n: string, a: number) {
-      this.name = n;
-      this.age = a;
-  }
-
-  greet(phrase: string) {
-      console.log(`${phrase} ${this.name}`);
-  }
+type User = {
+	firstName: string;
+	lastName: string;
+	age: number
 }
